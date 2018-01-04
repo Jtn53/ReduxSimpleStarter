3e99d64cb7d290223d4aae2cb2a27ab19b5aac2e
POF\jng <jng@pof.com>
2018-01-04 14:55:27 -0800

    Section 2 complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBliqxasGdp7PLh3BL_XJ4w8Y1H8sAAAAM';
 
 // This is a component that should create some HTML
@@ -11,19 +12,33 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'kripp'}, (videos) => {
-            this.setState({ videos });
+        this.videoSearch('kripp');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
             // above is the same as this.setState({ videos: videos });
         });
     }
     
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500);
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div> // <-- JSX (looks like HTML, but is Javascript)
         ); 
     }
